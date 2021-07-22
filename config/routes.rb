99068497096e107b200a961178387dec0444ac96
Morenikeji Bello <caeserselassie@gmail.com>
2021-07-22 20:17:43 +0100

    Add action and view for checkout

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,19 @@
 Rails.application.routes.draw do
+  get 'orders/index'
+  get 'orders/new'
+  get 'orders/create'
+  get 'orders/show'
+  get 'orders/edit'
+  get 'orders/update'
+  get 'orders/destroy'
   root 'products#index'
+
   resources :products, only: %i[show new create edit update]
+
   post '/products/add_cart', to: 'products#add_to_cart'
   get '/:id/cart', to: 'products#cart'
   post '/update_quantity', to: 'products#update_quantity'
-  # get '/checkout', to: 'product'
+  get '/checkout', to: 'orders#checkout'
 
 
   devise_for :users
