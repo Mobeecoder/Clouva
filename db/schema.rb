eebf965e9f8b2b0b22dac953e4dc8914aedec9ab
Morenikeji Bello <caeserselassie@gmail.com>
2021-07-25 12:28:58 +0100

    Dummy migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_25_085224) do
+ActiveRecord::Schema.define(version: 2021_07_25_110841) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
